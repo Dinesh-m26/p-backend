37dd5905d009416a28d56f8782bf25a9e77baa6d
dinesh <dineshubli@gmail.com>
2024-07-16 14:31:08 +0530

    14.31

--- index.js
+++ index.js
@@ -7,29 +7,33 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-    origin: 'https://mdinesh.netlify.app', // Allow requests from your Netlify app
-    methods: ['GET', 'POST'], // Allowed methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-    credentials: true // If needed for cookies or other credentials
-}));
-app.options('*', cors());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 // Database connection
 const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI)
-    .then(() => console.log("Connected to DB"))
-    .catch((error) => console.error("DB connection error:", error));
+.then(() => console.log("Connected to DB"))
+.catch((error) => console.error("DB connection error:", error));
 
 // Schema
 const formSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    message: { type: String, required: true }
+    name: {
+        required: true,
+        type: String,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: mongoose.Schema.Types.Mixed,
+        required: true,
+    }
 });
 
 // Model
-const formModel = mongoose.model('Form', formSchema);
+const formModel = mongoose.model('data', formSchema);
 
 // Post data
 app.post('/form', async (req, res) => {
